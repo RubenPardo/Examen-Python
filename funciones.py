ce9f5701b190562e34c3ca17f52922de539f58ba
Ruben <rubenpardocasanova@gmail.com>
2022-03-07 12:33:14 +0100

    entrega final

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+from calendar import c
 import random
 
 def encontrar_menores(diccionario,letra):
@@ -10,6 +11,7 @@ def encontrar_menores(diccionario,letra):
     """
     # el error se daba porque se inicializa el array  que se devuelve cada vez que se añade una palabra, por lo que
     # el resultado solo era la ultima palabra
+    # el array debe estar fuera del bucle, local a nivel de funcion no del if
     resultado=[]
     for clave in diccionario:
         for palabra in diccionario[clave]:
@@ -29,6 +31,7 @@ def add_client(clients_list,nif,name,address,phone,email):
     """
     # se estaba poniendo dentro del valor del diccionario una estructura que no esperaba el test
     # el test esperaba Nif -> {name:"",---} se le estaba devolviendo Nif -> {Nif:{name:"",---}}
+    # se soluciono cambiando la estrcutura
     clients_list[nif] = {'name': name,
               'address': address,
               'phone': phone,
@@ -46,13 +49,20 @@ def repartir_cartas(cartas_iniciales,repeticiones):
     """    
     combinaciones={}
     for i in range(1,repeticiones+1):
-        cartas_aleatorias = cartas_iniciales 
+        # el error se daba al igualar el array cartas_aleatorias con cartas_iniciales, 
+        # no se estaba copiando el array sino igualando la referencia, por lo que al borrar en una
+        # se borraba en la otra y no se reiniciaban las cartas 
+        # La solucion es copiar el array en vez de igualarlo
+        cartas_aleatorias = []
+        cartas_aleatorias = cartas_iniciales.copy() 
         combinaciones["repeticion"+str(i)]=[]
         for j in range(0,5):
+            print(cartas_aleatorias)
             carta=random.choice(cartas_aleatorias)
             combinaciones["repeticion"+str(i)].append(carta)
             cartas_aleatorias.remove(carta)
 
+    print(combinaciones)
     return combinaciones
 
     
